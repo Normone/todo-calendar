969f2bb3737720ec4b3f1efeea7d8b05bdd2f653
Norm One <vashhekin00@gmail.com>
2024-07-29 04:13:14 +0700

    Не тот рут

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-RSP-test/', // Добавил для работы gh-pages
+  base: '/todo-calendar/', // Добавил для работы gh-pages
   plugins: [
     {
       name: "treat-js-files-as-jsx",
